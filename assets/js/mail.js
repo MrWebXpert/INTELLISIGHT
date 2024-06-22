35dffa54b545c4f87e1958f229cc805a60022c92
MrWebXpert <mrwebxpert@gmail.com>
2024-06-22 22:14:01 +0500

    adding email functionality

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -9,73 +9,18 @@ function enableBtn() {
     btn.disabled = false;
     btn.innerText = 'Send Message';
 }
-// send mailForm 
 function sendMail() {
-    let sname = document.getElementById('name').value
-    let phoneNumber = document.getElementById('phone').value
-    let email = document.getElementById('email').value
-    let dvr = document.getElementById('dvr').value
-    let camera = document.getElementById('camera').value
-    let message = document.getElementById('message').value
-
-    let emailbody = `
-                    <td class="esd-container-frame" width="558" align="left" style="width: 558px; text-align: left;">
-                        <table width="100%" cellspacing="0" cellpadding="0" style="width: 100%; border-collapse: collapse;">
-                            <tbody>
-                                <tr>
-                                    <td class="esd-block-text" align="left" style="text-align: left;">
-                                        <p style="color: #999999;  margin: 0;">Message send By : ${sname}</p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="esd-block-text es-m-txt-c" align="left" style="text-align: left;">
-                                        <p style="margin: 0;"><b>Email :</b> ${email}</p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="esd-block-text es-m-txt-c" align="left" style="text-align: left;">
-                                        <p style="margin: 0;"><b>Phone Number : </b>${dvr},</p>
-                                        <hr>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="esd-block-text es-m-txt-c" align="left" style="text-align: left;">
-                                        <p style="margin: 0;"><b>Phone Number : </b>${camera},</p>
-                                        <hr>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="esd-block-text es-m-txt-c" align="left" style="text-align: left;">
-                                        <p style="margin: 0;"><b>Phone Number : </b>${phoneNumber},</p>
-                                        <hr>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="esd-block-text es-m-txt-c es-p10t" align="left" style="text-align: left; padding-top: 10px;">
-                                        <p>
-                                        Hi...
-                                        <br>
-                                        <span style="margin: 0 0 0 10px;"> ${message}</span> 
-                                       </p>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </td>
-                 `
-
-    Email.send({
-        Host: "smtp.elasticemail.com",
-        Username: "ahsrsolution@gmail.com",
-        Password: "956990C8E1A4FBF519019F6698C19CBAC9D7",
-        To: 'mrwebxpert@gmail.com',
-        // To: 'sales@ahsr-solutions.com',
-        From: `ahsrsolution@gmail.com`,
-        Subject: `New message from ${sname}`,
-        Body: emailbody
-    }).then((message) => {
+    let params = {
+        sname: document.getElementById('name').value,
+        phone: document.getElementById('phone').value,
+        email: document.getElementById('email').value,
+        dvr: document.getElementById('dvr').value,
+        camera: document.getElementById('camera').value,
+        message: document.getElementById('message').value,
+    }
+    emailjs.send("service_ts3hf7n", "template_7p0ffj6", params).then((message) => {
         console.log(message)
-        if (message === 'OK') {
+        if (message.text === 'OK') {
             Swal.fire({
                 title: `Thanks for your Message!`,
                 text: "A member of our team will reach out to you soon!",
@@ -97,21 +42,20 @@ function sendMail() {
             enableBtn()
         }
     }).catch((err) => {
+        console.log(err)
         Swal.fire({
             title: `Error Occured ${err}`,
             text: "Please click the whatsapp icon to contact us Thanks!",
             icon: "error"
         });
+
         enableBtn()
     })
-
-
 }
 
-
-
-let mailForm = document.getElementById("mailForm");
-mailForm.addEventListener("submit", (e) => {
+const form = document.getElementById("mailForm");
+console.log("here is form deatils", form)
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     sendMail()
 });
